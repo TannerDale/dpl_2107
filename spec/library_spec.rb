9b856b99d8b4055c6a7b656990bb50d27b0d8934
Tanner Dale <tanner@tannerdale.com>
2021-07-29 15:03:13 -0500

    Add iteration 4 library tests

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -59,4 +59,74 @@ RSpec.describe Author do
       expect(dpl.publication_time_frame_for(harper_lee)).to eq(expected_2)
     end
   end
+
+  context 'checking out books' do
+    dpl = Library.new("Denver Public Library")
+
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    villette = charlotte_bronte.write("Villette", "1853")
+    harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
+    mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+
+    dpl.add_author(charlotte_bronte)
+    dpl.add_author(harper_lee)
+
+    it 'cannot checkout a book it doesnt have' do
+      test_book = Book.new({author_first_name: "Mike", author_last_name: "Dao", title: "Why VIM is the Best IDE", publication_date: "July 4, 2021"})
+
+      expect(dpl.checkout(test_book)).to be(false)
+    end
+
+    it 'can checkout and return a book' do
+      result = dpl.checkout(jane_eyre)
+      dpl.checkout(mockingbird)
+
+      expect(result).to be(true)
+      expect(dpl.checked_out_books).to eq([jane_eyre, mockingbird])
+
+      dpl.return(jane_eyre)
+
+      expect(dpl.checked_out_books).to eq([mockingbird])
+
+      dpl.return(mockingbird)
+
+      expect(dpl.checked_out_books).to eq([])
+    end
+
+    it 'cannot checkout an already checked out book' do
+      dpl.checkout(jane_eyre)
+      result = dpl.checkout(jane_eyre)
+
+      expect(result).to be(false)
+      expect(dpl.checked_out_books).to eq([jane_eyre])
+
+      dpl.return(jane_eyre)
+    end
+  end
+
+  context 'most popular book' do
+    dpl = Library.new("Denver Public Library")
+
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    villette = charlotte_bronte.write("Villette", "1853")
+    harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
+    mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+
+    dpl.add_author(charlotte_bronte)
+    dpl.add_author(harper_lee)
+    dpl.checkout(jane_eyre)
+
+    dpl.checkout(villette)
+    dpl.checkout(mockingbird)
+    dpl.return(mockingbird)
+    dpl.checkout(mockingbird)
+    dpl.return(mockingbird)
+    dpl.checkout(mockingbird)
+
+    it 'has a most popular book' do
+      expect(dpl.most_popular_book).to eq(mockingbird)
+    end
+  end
 end
