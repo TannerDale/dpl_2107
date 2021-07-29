c28ccd8d7c0ad4a2d4802a6fca63ceb2dc04f28b
Tanner Dale <tanner@tannerdale.com>
2021-07-29 14:23:37 -0500

    Add author class

---
+++ lib/author.rb
@@ -0,0 +1,24 @@
+class Author
+  attr_reader :books
+
+  def initialize(params)
+    @first_name = params[:first_name]
+    @last_name = params[:last_name]
+    @books = []
+  end
+
+  def name
+    "#{@first_name} #{@last_name}"
+  end
+
+  def write(title, year)
+    book = Book.new({
+      author_first_name: @first_name,
+      author_last_name: @last_name,
+      title: title,
+      publication_date: year,
+    })
+    @books << book
+    book
+  end
+end
