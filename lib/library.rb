493fd6b7a8f86ffe209b150d9bf82f1b1b14c92c
Tanner Dale <tanner@tannerdale.com>
2021-07-29 15:15:48 -0500

    Add Iteration 4 library methods

--- lib/library.rb
+++ lib/library.rb
@@ -1,10 +1,11 @@
 class Library
-  attr_reader :name, :authors, :books
+  attr_reader :name, :authors, :books, :checked_out_books
 
   def initialize(name)
     @name = name
     @authors = []
     @books = []
+    @checked_out_books = []
   end
 
   def add_author(author)
@@ -24,5 +25,32 @@ class Library
       start: first.publication_year,
       end: last.publication_year
     }
+  end
+
+  def checkout(book)
+    book.get_checked_out
+    if has_book?(book)
+      @checked_out_books << book
+      return true
+    end
+    false
+  end
+
+  def return(book)
+    @checked_out_books.delete(book)
+  end
+
+  def has_book?(book)
+    @books.include?(book) && not_checked_out?(book)
+  end
+
+  def not_checked_out?(book)
+    !@checked_out_books.include?(book)
+  end
+
+  def most_popular_book
+    @books.max_by do |book|
+      book.times_checked_out
+    end
   end
 end
