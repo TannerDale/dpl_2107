65fcd46ac52697da194ae9fe44d3f36c44c138f0
Tanner Dale <tanner@tannerdale.com>
2021-07-29 15:35:35 -0500

    Add behavior context block, add get_checked_out test

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -12,10 +12,21 @@ RSpec.describe Book do
       expect(book.title).to eq("To Kill a Mockingbird")
       expect(book.author).to eq("Harper Lee")
       expect(book.publication_year).to eq("1960")
+      expect(book.times_checked_out).to eq(0)
     end
+  end
+
+  context 'behavior' do
+    book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
 
     it 'can get a year from a date' do
       expect(book.get_publication_year("July 11, 1960")).to eq("1960")
+    end
+
+    it 'keep track of times checked out' do
+      book.get_checked_out
+
+      expect(book.times_checked_out).to eq(1)
     end
   end
 end
