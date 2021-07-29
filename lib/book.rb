ba5d5063565a91cc02ea409d46db6a2595ebbcc0
Tanner Dale <tanner@tannerdale.com>
2021-07-29 14:48:03 -0500

    Refactor author name logic

--- lib/book.rb
+++ lib/book.rb
@@ -1,18 +1,18 @@
 class Book
-  attr_reader :title, :publication_year
+  attr_reader :title, :author, :publication_year, :times_checked_out
 
   def initialize(params)
     @title = params[:title]
-    @author_first_name = params[:author_first_name]
-    @author_last_name = params[:author_last_name]
+    @author = "#{params[:author_first_name]} #{params[:author_last_name]}"
     @publication_year = get_publication_year(params[:publication_date])
-  end
-
-  def author
-    "#{@author_first_name} #{@author_last_name}"
+    @times_checked_out = 0
   end
 
   def get_publication_year(date)
     date.split.last
+  end
+
+  def get_checked_out
+    @times_checked_out += 1
   end
 end
